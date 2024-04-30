d4f611c2c4251665b07b9bab1591fd9d95c098bd
Nolan Pollack <nolanfpollack@gmail.com>
2024-04-30 00:28:06 -0400

    nvim cmp

--- Mackup/.config/nvim/lua/nolan/plugins/nvim-cmp.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/nvim-cmp.lua
@@ -3,5 +3,14 @@ return {
 	event = "InsertEnter",
 	dependencies = {
 		"hrsh7th/cmp-buffer",
-	}
+		"hrsh7th/cmp-path",
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+		}
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
+	},
 }
