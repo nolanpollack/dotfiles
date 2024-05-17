8623a525f7b208d6a0ef469e546c11cb18659e6b
Nolan Pollack <nolanfpollack@gmail.com>
2024-05-17 02:18:26 -0400

    autoinstall for treesitter

--- Mackup/.config/nvim/lua/nolan/plugins/treesitter.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/treesitter.lua
@@ -1,64 +1,41 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-    },
-    config = function()
-        -- import nvim-treesitter plugin
-        local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
 
-        -- configure treesitter
-        treesitter.setup({ -- enable syntax highlighting
-            highlight = {
-                enable = true,
-            },
-            -- enable indentation
-            indent = { enable = true },
-            -- enable autotagging (w/ nvim-ts-autotag plugin)
-            autotag = {
-                enable = true,
-            },
-            -- ensure these language parsers are installed
-            ensure_installed = {
-                "bash",
-                "c",
-                "c_sharp",
-                "css",
-                "dockerfile",
-                "git_config",
-                "gitignore",
-                "graphql",
-                "html",
-                "http",
-                "java",
-                "javascript",
-                "json",
-                "kotlin",
-                "lua",
-                "make",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "requirements",
-                "sql",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
-        })
-    end,
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			-- automatically install missing parsers when entering buffer
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+
+		-- set keymap
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ti", "<cmd>InspectTree<CR>", { desc = "Inspect treesitter nodes" })
+	end,
 }
