4bac5754d8f99916ceb6e8fad2095e7c062e21ca
Nolan Pollack <nolanfpollack@gmail.com>
2024-05-19 01:33:40 -0400

    bufferline offset for tree and bb to switch buffers

--- Mackup/.config/nvim/lua/nolan/plugins/bufferline.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/bufferline.lua
@@ -9,7 +9,27 @@ return {
 			highlights = require("catppuccin.groups.integrations.bufferline").get(),
 			options = {
 				diagnostics = "nvim_lsp",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true, -- use a "true" to enable the default, or set your own character
+					},
+				},
 			},
 		})
+
+		vim.keymap.set(
+			{
+				"n",
+				"v",
+			},
+			"<leader>bb",
+			function()
+				require("bufferline").pick_buffer()
+			end,
+			{ desc = "Pick buffer" }
+		)
 	end,
 }
