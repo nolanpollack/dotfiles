53684947f7dec4aff339008bad8be7097b294fd8
Nolan Pollack <nolanfpollack@gmail.com>
2024-05-21 00:18:14 -0400

    lsp keybinds

--- Mackup/.config/nvim/lua/nolan/plugins/lsp/lspconfig.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/lsp/lspconfig.lua
@@ -26,9 +26,9 @@ return {
 				local opts = { buffer = ev.buf, silent = true }
 
 				-- set keybinds
-				-- opts.desc = "Show LSP references"
-				-- keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-				--
+				opts.desc = "Show LSP references"
+				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
 				-- opts.desc = "Go to declaration"
 				-- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 				--
@@ -40,10 +40,10 @@ return {
 				--
 				-- opts.desc = "Show LSP type definitions"
 				-- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-				--
-				-- opts.desc = "See available code actions"
-				-- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-				--
+
+				opts.desc = "See available code actions"
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
 				-- opts.desc = "Smart rename"
 				-- keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 				--
@@ -58,9 +58,6 @@ return {
 				--
 				-- opts.desc = "Go to next diagnostic"
 				-- keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-				--
-				-- opts.desc = "Show documentation for what is under cursor"
-				-- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 				--
 				-- opts.desc = "Restart LSP"
 				-- keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
