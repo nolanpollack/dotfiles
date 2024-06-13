901ab8846cc4541c74ad232d63c54a5c1620bc95
Nolan Pollack <nolanfpollack@gmail.com>
2024-06-13 15:24:46 -0400

    eza

--- Mackup/.zsh/aliases.zsh
+++ Mackup/.zsh/aliases.zsh
@@ -1,2 +1,7 @@
 alias vim=nvim
 alias prettier="npx prettier"
+
+# eza
+alias ls="eza --group-directories-first --icons=auto"
+alias l="eza -al --icons=auto --group-directories-first"
+alias lt="eza -T --icons=auto"
