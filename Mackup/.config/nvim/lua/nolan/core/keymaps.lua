36156c1495e8225d1fbba0739a42085be791acc1
Nolan Pollack <nolanfpollack@gmail.com>
2024-06-17 14:16:34 -0400

    keymaps update

--- Mackup/.config/nvim/lua/nolan/core/keymaps.lua
+++ Mackup/.config/nvim/lua/nolan/core/keymaps.lua
@@ -12,3 +12,18 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down in visual mode" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up in visual mode" })
+
+-- Scrolling
+keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Better half up scroll", remap = true })
+keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Better half down scroll", remap = true })
+
+keymap.set("n", "<leader>a", "gg<S-v>G", { desc = "Select all" })
+
+-- Move in insert mode, And Command-line mode
+keymap.set({ "i", "c" }, "<C-j>", "<Down>", { desc = "Move Down in insert mode" })
+keymap.set({ "i", "c" }, "<C-k>", "<Up>", { desc = "Move Up in insert mode" })
+keymap.set({ "i", "c" }, "<C-h>", "<Left>", { desc = "Move Left in insert mode" })
+keymap.set({ "i", "c" }, "<C-l>", "<Right>", { desc = "Move Right in insert mode" })
