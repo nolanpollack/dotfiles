bf76df49058c62694d6dba670689eae5ebd906ae
Nolan Pollack <nolanfpollack@gmail.com>
2024-06-25 15:49:04 -0400

    ? for whichkey

--- Mackup/.config/nvim/lua/nolan/plugins/which-key.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/which-key.lua
@@ -4,8 +4,7 @@ return {
 	init = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
+		vim.keymap.set("n", "?", ":WhichKey<CR>", { desc = "WhichKey" })
 	end,
-	opts = {
-
-	}
+	opts = {},
 }
