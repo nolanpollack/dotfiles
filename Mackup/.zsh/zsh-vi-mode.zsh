68fe9190c49d5af86c4dbef8527c14b24f33d2ab
Nolan Pollack <nolanfpollack@gmail.com>
2024-06-27 12:17:01 -0400

    fix fzf

--- Mackup/.zsh/zsh-vi-mode.zsh
+++ Mackup/.zsh/zsh-vi-mode.zsh
@@ -1,2 +1,6 @@
 ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+
+# Allow fzf to override the default key bindings
+zvm_after_init_commands+=('source <(fzf --zsh)')
+
 source /home/linuxbrew/.linuxbrew/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
