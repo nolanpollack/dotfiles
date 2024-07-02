e41398ce5d352d52cfe9710e8d0f951ed93ddb76
Nolan Pollack <nolanfpollack@gmail.com>
2024-07-02 19:21:20 -0400

    fix python linting errors with venv

--- Mackup/.config/nvim/lua/nolan/plugins/linting.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/linting.lua
@@ -12,6 +12,9 @@ return {
 			python = { "pylint" },
 		}
 
+		lint.linters.pylint.cmd = "python"
+		lint.linters.pylint.args = { "-m", "pylint", "-f", "json" }
+
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
