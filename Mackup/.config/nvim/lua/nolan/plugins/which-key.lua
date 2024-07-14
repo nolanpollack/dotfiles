767ed533131b067ca89ccb6af9320c70aef832c4
Nolan Pollack <nolanfpollack@gmail.com>
2024-07-14 18:07:48 -0400

    update which-key

--- Mackup/.config/nvim/lua/nolan/plugins/which-key.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/which-key.lua
@@ -1,10 +1,14 @@
 return {
 	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-		vim.keymap.set("n", "?", ":WhichKey<CR>", { desc = "WhichKey" })
-	end,
+	evet = "VeryLazy",
 	opts = {},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
