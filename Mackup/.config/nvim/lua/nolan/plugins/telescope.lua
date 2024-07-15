67ad98b0301a1dcf0dafc06953f1a4641be5b2fc
Nolan Pollack <nolanfpollack@gmail.com>
2024-07-15 12:29:27 -0400

    more telescope pickers

--- Mackup/.config/nvim/lua/nolan/plugins/telescope.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
 		"folke/todo-comments.nvim",
 		"kkharji/sqlite.lua",
 		"danielfalk/smart-open.nvim",
+		"olacin/telescope-gitmoji.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
@@ -37,14 +38,17 @@ return {
 
 		telescope.load_extension("fzf")
 		telescope.load_extension("smart_open")
+		telescope.load_extension("gitmoji")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find files" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find String in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under Cursor in cwd" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find Todos" })
+		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find Buffers" })
+		keymap.set("n", "<leader>fg", "<cmd>Telescope gitmoji<cr>", { desc = "Find Gitmoji for commits" })
+		keymap.set("n", "<leader>fp", "<cmd>Telescope builtin<cr>", { desc = "Find telescope Pickers" })
 		keymap.set("n", "<leader><leader>", "<cmd>Telescope smart_open<cr>", { desc = "Smart open files" })
 	end,
 }
