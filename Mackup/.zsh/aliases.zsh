6fde7183b603df5a5b23e7b1236477d6ec3668c3
Nolan Pollack <nolanfpollack@gmail.com>
2024-07-27 20:57:58 -0400

    zoxide alias

--- Mackup/.zsh/aliases.zsh
+++ Mackup/.zsh/aliases.zsh
@@ -6,6 +6,9 @@ alias ls="eza --group-directories-first --icons=auto"
 alias l="eza -al --icons=auto --group-directories-first"
 alias lt="eza -T --icons=auto"
 
+# zoxide
+alias cd="z"
+
 # bat
 alias batlog="bat --paging=never -l log"
 
