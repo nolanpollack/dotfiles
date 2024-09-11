75c3c84e488150a7b17b95a0151f13ed25696a7c
Nolan Pollack <nolanfpollack@gmail.com>
2024-09-11 17:33:32 -0400

    fix enter on rf

--- Mackup/.zsh/fzf.zsh
+++ Mackup/.zsh/fzf.zsh
@@ -21,6 +21,6 @@ fzf_ripgrep() {
       --delimiter : \
       --preview 'bat --color=always {1} --highlight-line {2}' \
       --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-      --bind 'enter:become(vim {1} +{2})'
+      --bind 'enter:become(nvim {1} +{2})'
 }
 alias rf=fzf_ripgrep
