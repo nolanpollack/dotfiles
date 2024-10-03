5b10e82f509f1073fadff6e35cef3ce198dca173
Nolan Pollack <nolanfpollack@gmail.com>
2024-10-03 10:03:59 -0400

    customize vi mode highlight

--- Mackup/.zsh/zsh-vi-mode.zsh
+++ Mackup/.zsh/zsh-vi-mode.zsh
@@ -1,4 +1,6 @@
 ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+ZVM_VI_HIGHLIGHT_BACKGROUND=#313244
+ZVM_VI_HIGHLIGHT_FOREGROUND=white
 
 # Allow fzf to override the default key bindings
 zvm_after_init_commands+=('source <(fzf --zsh)')
