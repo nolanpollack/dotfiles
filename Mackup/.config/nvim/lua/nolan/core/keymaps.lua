47b6a571683ee5e84375b433ddfe363d6ce059cf
Nolan Pollack <nolanfpollack@gmail.com>
2024-10-14 19:22:39 -0400

    Swap window key

--- Mackup/.config/nvim/lua/nolan/core/keymaps.lua
+++ Mackup/.config/nvim/lua/nolan/core/keymaps.lua
@@ -12,6 +12,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>ss", "<C-w>x", { desc = "Swap windows" }) -- swap windows
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down in visual mode" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up in visual mode" })
