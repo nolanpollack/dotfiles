9b14e679d9922b87b8d9d73a8e1be530953a6fae
Nolan Pollack <nolanfpollack@gmail.com>
2024-10-31 15:05:43 -0400

    better format for diffview

--- Mackup/.config/nvim/lua/nolan/plugins/diffview.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/diffview.lua
@@ -1,16 +1,15 @@
 return {
 	"sindrets/diffview.nvim",
-	config = function()
-		require("diffview").setup({})
-		vim.keymap.set("n", "<leader>gdo", "<cmd>DiffviewOpen<CR>", { desc = "Open diffview" })
-		vim.keymap.set("n", "<leader>gdc", "<cmd>DiffviewClose<CR>", { desc = "Close diffview" })
-		vim.keymap.set("n", "<leader>gdh", "<cmd>DiffviewFileHistory<CR>", { desc = "Open diffview for file history" })
-		vim.keymap.set("n", "<leader>gdf", "<cmd>DiffviewFileHistory %<CR>", { desc = "View history for this file" })
-		vim.keymap.set(
-			"n",
+	opts = {},
+	keys = {
+		{ "<leader>gdo", "<cmd>DiffviewOpen<CR>", desc = "Open diffview" },
+		{ "<leader>gdc", "<cmd>DiffviewClose<CR>", desc = "Close diffview" },
+		{ "<leader>gdh", "<cmd>DiffviewFileHistory<CR>", desc = "Open diffview for file history" },
+		{ "<leader>gdf", "<cmd>DiffviewFileHistory %<CR>", desc = "View history for this file" },
+		{
 			"<leader>gdb",
 			"<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<CR>",
-			{ desc = "View changes made to this branch" }
-		)
-	end,
+			desc = "View changes made to this branch",
+		},
+	},
 }
