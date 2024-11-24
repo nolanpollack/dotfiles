406f561f0902155d35456edd32b418846ea1a5a1
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-23 22:15:40 -0500

    remove unecessary highlights

--- Mackup/.config/nvim/lua/nolan/plugins/colorscheme.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/colorscheme.lua
@@ -12,21 +12,6 @@ return {
 				fidget = true,
 				noice = true,
 			},
-			custom_highlights = function(colors)
-				return {
-					GitGraphHash = { fg = colors.mauve },
-					GitGraphTimestamp = { fg = colors.peach },
-					GitGraphAuthor = { fg = colors.blue },
-					GitGraphBranchName = { fg = colors.pink },
-					GitGraphBranchTag = { fg = colors.red },
-					GitGraphBranchMsg = { fg = colors.text },
-					GitGraphBranch1 = { fg = colors.red },
-					GitGraphBranch2 = { fg = colors.green },
-					GitGraphBranch3 = { fg = colors.blue },
-					GitGraphBranch4 = { fg = colors.yellow },
-					GitGraphBranch5 = { fg = colors.mauve },
-				}
-			end,
 		})
 
 		vim.cmd.colorscheme("catppuccin")
