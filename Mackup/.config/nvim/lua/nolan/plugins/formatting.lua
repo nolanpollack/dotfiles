7a4cc19c3869635984f055963e9699c8f32fa587
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-23 23:53:54 -0500

    Remove format on save

--- Mackup/.config/nvim/lua/nolan/plugins/formatting.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/formatting.lua
@@ -1,5 +1,19 @@
 return {
 	"stevearc/conform.nvim",
+	keys = {
+		{
+			"<leader>mp",
+			function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = true,
+					timeout_ms = 1000,
+				})
+			end,
+			{ "n", "v" },
+			{ desc = "Make Pretty (Format file or range in visual mode)" },
+		},
+	},
 	opts = function()
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "java",
@@ -24,25 +38,6 @@ return {
 				typescriptreact = { "prettier" },
 				yaml = { "prettier" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
 		}
 	end,
-	keys = {
-		{
-			"<leader>mp",
-			function()
-				require("conform").format({
-					lsp_fallback = true,
-					async = true,
-					timeout_ms = 1000,
-				})
-			end,
-			{ "n", "v" },
-			{ desc = "Make Pretty (Format file or range in visual mode)" },
-		},
-	},
 }
