b7ec3d8e5ecc2bc1ae543cca45ba23cf6d966a1a
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-23 22:15:58 -0500

    Use opts for comment

--- Mackup/.config/nvim/lua/nolan/plugins/comment.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/comment.lua
@@ -4,16 +4,13 @@ return {
 	dependencies = {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
-	config = function()
-		-- import comment plugin safely
-		local comment = require("Comment")
-
+	opts = function()
 		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
 		-- enable comment
-		comment.setup({
+		return {
 			-- for commenting tsx, jsx, svelte, html files
 			pre_hook = ts_context_commentstring.create_pre_hook(),
-		})
+		}
 	end,
 }
