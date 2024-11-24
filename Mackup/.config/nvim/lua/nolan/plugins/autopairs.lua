d99b6fc94e0852eb6df95c5067f5ecf6d63f55cd
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-23 22:08:01 -0500

    check pairs in java with autopairs

--- Mackup/.config/nvim/lua/nolan/plugins/autopairs.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/autopairs.lua
@@ -4,20 +4,7 @@ return {
 	dependencies = {
 		"hrsh7th/nvim-cmp",
 	},
-	config = function()
-		-- import nvim-autopairs
-		local autopairs = require("nvim-autopairs")
-
-		-- configure autopairs
-		autopairs.setup({
-			check_ts = true, -- enable treesitter
-			ts_config = {
-				lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-				javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-				java = false, -- don't check treesitter on java
-			},
-		})
-
+	opts = function()
 		-- import nvim-autopairs completion functionality
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
@@ -26,5 +13,14 @@ return {
 
 		-- make autopairs and completion work together
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+		-- configure autopairs
+		return {
+			check_ts = true, -- enable treesitter
+			ts_config = {
+				lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+				javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+			},
+		}
 	end,
 }
