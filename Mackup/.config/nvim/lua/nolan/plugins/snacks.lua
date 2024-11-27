f4bdc321768f13ebfeb894789d86458963ed9ef8
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-26 22:39:15 -0500

    Snacks plugin

---
+++ Mackup/.config/nvim/lua/nolan/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- Setup some globals for debugging (lazy-loaded)
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+				vim.print = _G.dd -- Override print to use snacks for `:=` command
+			end,
+		})
+	end,
+	lazy = false,
+	opts = {},
+}
