6c667b68d3fe0b7c9b7b6c5ddfc87adb714823b6
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-28 11:17:58 -0500

    dap ui

--- Mackup/.config/nvim/lua/nolan/plugins/lsp/dap.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/lsp/dap.lua
@@ -1,19 +1,42 @@
 return {
-	"mfussenegger/nvim-dap",
-	keys = {
-		{
-			"<leader>rd",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Run debug",
+	{
+		"mfussenegger/nvim-dap",
+		keys = {
+			{
+				"<leader>rd",
+				function()
+					require("dap").continue()
+				end,
+				desc = "run debug",
+			},
+			{
+				"<leader>bp",
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				desc = "toggle breakpoint",
+			},
 		},
-		{
-			"<leader>bp",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "Toggle breakpoint",
-		},
+		config = function()
+			local sign = vim.fn.sign_define
+
+			sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+			sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+			sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
 	},
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 }
