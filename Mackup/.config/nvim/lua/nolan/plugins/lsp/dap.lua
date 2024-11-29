34284a0b9ef24024034858bed69ae33705ab9fb0
Nolan Pollack <nolanfpollack@gmail.com>
2024-11-29 00:35:21 -0500

    more dap bindings

--- Mackup/.config/nvim/lua/nolan/plugins/lsp/dap.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/lsp/dap.lua
@@ -3,18 +3,32 @@ return {
 		"mfussenegger/nvim-dap",
 		keys = {
 			{
-				"<leader>rd",
+				"<leader>dc",
 				function()
 					require("dap").continue()
 				end,
-				desc = "run debug",
+				desc = "Debug continue",
 			},
 			{
-				"<leader>bp",
+				"<leader>db",
 				function()
 					require("dap").toggle_breakpoint()
 				end,
-				desc = "toggle breakpoint",
+				desc = "Toggle breakpoint",
+			},
+			{
+				"<leader>do",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "Debug step over",
+			},
+			{
+				"<leader>di",
+				function()
+					require("dap").step_into()
+				end,
+				desc = "Debug step into",
 			},
 		},
 		config = function()
@@ -23,19 +37,21 @@ return {
 			sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
 			sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
 			sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
-			local dap, dapui = require("dap"), require("dapui")
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+
+			local dap, dapui, wk = require("dap"), require("dapui"), require("which-key")
+			wk.add({ "<leader>d", group = "Debug" })
+			-- dap.listeners.before.attach.dapui_config = function()
+			-- 	dapui.open()
+			-- end
+			-- dap.listeners.before.launch.dapui_config = function()
+			-- 	dapui.open()
+			-- end
+			-- dap.listeners.before.event_terminated.dapui_config = function()
+			-- 	dapui.close()
+			-- end
+			-- dap.listeners.before.event_exited.dapui_config = function()
+			-- 	dapui.close()
+			-- end
 		end,
 	},
 	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
