95382f12c65b3415a375fcd7c8d972482dcff9e3
Nolan Pollack <nolanfpollack@gmail.com>
2024-12-01 20:59:38 -0500

    Format lsp log messages

--- Mackup/.config/nvim/lua/nolan/plugins/lsp/lspconfig.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/lsp/lspconfig.lua
@@ -20,6 +20,8 @@ return {
 
 		vim.lsp.inlay_hint.enable()
 
+        vim.lsp.log.set_format_func(vim.inspect)
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
