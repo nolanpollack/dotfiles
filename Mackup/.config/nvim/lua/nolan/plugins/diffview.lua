a6fc40f4c62cebb7cee01fb303d683ec0128e54d
Nolan Pollack <nolanfpollack@gmail.com>
2024-12-01 20:54:45 -0500

    Better git diff keybindings

--- Mackup/.config/nvim/lua/nolan/plugins/diffview.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/diffview.lua
@@ -1,15 +1,47 @@
 return {
 	"sindrets/diffview.nvim",
-	opts = {},
+	opts = {
+		keymaps = {
+			view = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+            diff1 = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            diff2 = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            diff3 = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            diff4 = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            file_panel = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            file_history_panel = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            option_panel = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+            help_panel = {
+                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+            },
+		},
+	},
 	keys = {
-		{ "<leader>gdo", "<cmd>DiffviewOpen<CR>", desc = "Open diffview" },
-		{ "<leader>gdc", "<cmd>DiffviewClose<CR>", desc = "Close diffview" },
-		{ "<leader>gdh", "<cmd>DiffviewFileHistory<CR>", desc = "Open diffview for file history" },
-		{ "<leader>gdf", "<cmd>DiffviewFileHistory %<CR>", desc = "View history for this file" },
+		{ "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Open diffview" },
+		{ "<leader>gl", "<cmd>DiffviewFileHistory<CR>", desc = "Git log" },
+		{ "<leader>gh", "<cmd>DiffviewFileHistory %<CR>", desc = "View history for this file" },
 		{
-			"<leader>gdb",
+			"<leader>gr",
 			"<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<CR>",
 			desc = "View changes made to this branch",
 		},
 	},
+    setup = function()
+        require("diffview").setup()
+    end,
 }
