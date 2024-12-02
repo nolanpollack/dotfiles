1d9c6cdc1ccde94043ca5c7629153f66beef37ec
Nolan Pollack <nolanfpollack@gmail.com>
2024-12-01 21:32:53 -0500

    Git sign keybinds

--- Mackup/.config/nvim/lua/nolan/plugins/gitsigns.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/gitsigns.lua
@@ -1,4 +1,28 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	event = "BufRead",
+	keys = {
+		{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<CR>", desc = "Git blame" },
+		{
+			"ih",
+			mode = { "o", "x" },
+            ":<C-U>Gitsigns select_hunk<CR>",
+			desc = "inner hunk",
+		},
+		{
+			"<leader>gs",
+			function()
+				require("gitsigns").stage_hunk()
+			end,
+            desc = "Stage hunk",
+		},
+		{
+			"<leader>gu",
+			function()
+				require("gitsigns").reset_hunk()
+			end,
+            desc = "Unstage hunk",
+		},
+	},
 	opts = {},
 }
