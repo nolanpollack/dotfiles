49c8d0ae16ad5c67b2e6da4491edc6ba8da7b821
Nolan Pollack <nolanfpollack@gmail.com>
2024-12-24 13:43:42 -0500

    System clipboard for zsh-vi-mode

--- Mackup/.zsh/zsh-vi-mode.zsh
+++ Mackup/.zsh/zsh-vi-mode.zsh
@@ -2,6 +2,34 @@ ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
 ZVM_VI_HIGHLIGHT_BACKGROUND=#313244
 ZVM_VI_HIGHLIGHT_FOREGROUND=white
 
+# Use system clipboard
+function zvm_after_lazy_keybindings() {
+    source "$HOME/dotfiles/Mackup/.zsh/plugins/zsh-system-clipboard/zsh-system-clipboard.zsh"
+
+    functions -c zvm_yank zvm_yank_orig
+    function zvm_yank() {
+        zvm_yank_orig "$@"
+        printf '%s\n' "$CUTBUFFER" | zsh-system-clipboard-set
+    }
+
+    functions -c zvm_replace_selection zvm_replace_selection_orig
+    function zvm_replace_selection() {
+        zvm_replace_selection_orig "$@"
+        printf '%s\n' "$CUTBUFFER" | zsh-system-clipboard-set
+    }
+    
+    functions -c zvm_vi_put_after zvm_vi_put_after_orig
+    function zvm_vi_put_after() {
+        CUTBUFFER=$(zsh-system-clipboard-get)
+        zvm_vi_put_after_orig "$@"
+    }
+    functions -c zvm_vi_put_before zvm_vi_put_before_orig
+    function zvm_vi_put_before() {
+        CUTBUFFER=$(zsh-system-clipboard-get)
+        zvm_vi_put_before_orig "$@"
+    }
+}
+
 # Allow fzf to override the default key bindings
 zvm_after_init_commands+=('source <(fzf --zsh)')
 
