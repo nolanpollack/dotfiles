58400fdf317584ab59a9a62b87389572b83940a0
Nolan Pollack <nolanfpollack@gmail.com>
2025-01-25 15:42:22 -0500

    Perfect neovim config

--- Mackup/.config/nvim/lua/nolan/plugins/avante.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/avante.lua
@@ -7,8 +7,10 @@ return {
 		vim.treesitter.language.register("markdown", "Avante")
 	end,
 	opts = {
-		provider = "copilot",
-		auto_suggestions_provider = "copilot",
+		provider = "claude",
+		behavior = {
+			auto_suggestions = false,
+		},
 	},
 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
@@ -19,30 +21,6 @@ return {
 		--- The below dependencies are optional,
 		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
 		"echasnovski/mini.icons",
-		"zbirenbaum/copilot.lua", -- for providers='copilot'
-		{
-			-- support for image pasting
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				-- recommended settings
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					-- required for Windows users
-					use_absolute_path = true,
-				},
-			},
-		},
-		-- {
-		-- 	"MeanderingProgrammer/render-markdown.nvim",
-		-- 	opts = {
-		-- 		file_types = { "markdown", "Avante" },
-		-- 	},
-		-- 	ft = { "markdown", "Avante" },
-		-- },
+		"OXY2DEV/markview.nvim",
 	},
 }
