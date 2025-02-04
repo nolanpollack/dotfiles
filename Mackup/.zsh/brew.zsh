b44905167fd575fd4a00f433e86e1e4f25e38939
Nolan Pollack <nolanfpollack@gmail.com>
2025-02-04 16:17:09 -0500

    Let brew autocompletion work

--- Mackup/.zsh/brew.zsh
+++ Mackup/.zsh/brew.zsh
@@ -1,30 +1,32 @@
 export HOMEBREW_NO_ENV_HINTS=TRUE
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
-# save installed brew packages to requirements
-brew_install_and_update_requirements() {
-    if command brew install "$@"; then
-        brew leaves --installed-on-request > ~/dotfiles/brew_requirements.txt
-    fi
-}
-brew_uninstall_and_update_requirements() {
-    if command brew uninstall "$@"; then
-        brew leaves --installed-on-request > ~/dotfiles/brew_requirements.txt
-    fi
-}
-alias brew='brew_with_logging'
-brew_with_logging() {
-    case "$1" in
-        install)
-            shift
-            brew_install_and_update_requirements "$@"
-            ;;
-        uninstall)
-            shift
-            brew_uninstall_and_update_requirements "$@"
-            ;;
-        *)
-            command brew "$@"
-            ;;
-    esac
-}
+# brew_install_and_update_requirements() {
+#     if command brew install "$@"; then
+#         brew leaves --installed-on-request > ~/dotfiles/brew_requirements.txt
+#     fi
+# }
+# brew_uninstall_and_update_requirements() {
+#     if command brew uninstall "$@"; then
+#         brew leaves --installed-on-request > ~/dotfiles/brew_requirements.txt
+#     fi
+# }
+# alias brew='brew_with_logging'
+# brew_with_logging() {
+#     case "$1" in
+#         install)
+#             shift
+#             brew_install_and_update_requirements "$@"
+#             ;;
+#         uninstall)
+#             shift
+#             brew_uninstall_and_update_requirements "$@"
+#             ;;
+#         *)
+#             command brew "$@"
+#             ;;
+#     esac
+# }
+
+eval $(brew shellenv zsh)
+# Required for brew completions to work
+export FPATH="$HOMEBREW_PREFIX/share/zsh/site-functions:${FPATH:-}";
