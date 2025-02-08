a86d7849841821901f86a2a4f7886879722d36c4
Nolan Pollack <nolanfpollack@gmail.com>
2025-02-08 15:50:39 -0500

    Add mini-ai again

---
+++ Mackup/.config/nvim/lua/nolan/plugins/mini.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			symbol = "│",
+		},
+	},
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		opts = {},
+	},
+}
