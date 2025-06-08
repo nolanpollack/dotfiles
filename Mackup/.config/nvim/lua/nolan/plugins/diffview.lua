496a6610b44a752b09465744d527e854f8371872
Nolan Pollack <nolanfpollack@gmail.com>
2025-06-08 15:37:44 -0400

    clean diffview

--- Mackup/.config/nvim/lua/nolan/plugins/diffview.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/diffview.lua
@@ -5,30 +5,30 @@ return {
 			view = {
 				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
 			},
-            diff1 = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            diff2 = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            diff3 = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            diff4 = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            file_panel = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            file_history_panel = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            option_panel = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
-            help_panel = {
-                ["<leader>gd"] = "<cmd>DiffviewClose<CR>",
-            },
+			diff1 = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			diff2 = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			diff3 = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			diff4 = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			file_panel = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			file_history_panel = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			option_panel = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
+			help_panel = {
+				["<leader>gd"] = "<cmd>DiffviewClose<CR>",
+			},
 		},
 	},
 	keys = {
@@ -41,7 +41,4 @@ return {
 			desc = "View changes made to this branch",
 		},
 	},
-    setup = function()
-        require("diffview").setup()
-    end,
 }
