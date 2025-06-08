9bae6b815d6a34082e09e8283ed251c5fe534735
Nolan Pollack <nolanfpollack@gmail.com>
2025-06-08 15:54:17 -0400

    Git open command

--- Mackup/.config/nvim/lua/nolan/plugins/snacks.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/snacks.lua
@@ -6,16 +6,26 @@ return {
 			pattern = "VeryLazy",
 			callback = function()
 				-- Setup some globals for debugging (lazy-loaded)
+				local snacks = require("snacks")
 				_G.dd = function(...)
-					Snacks.debug.inspect(...)
+					snacks.debug.inspect(...)
 				end
 				_G.bt = function()
-					Snacks.debug.backtrace()
+					snacks.debug.backtrace()
 				end
 				vim.print = _G.dd -- Override print to use snacks for `:=` command
 			end,
 		})
 	end,
+	keys = {
+		{
+			"<leader>go",
+			function()
+				require("snacks").gitbrowse()
+			end,
+			desc = "Open current file in browser",
+		},
+	},
 	lazy = false,
 	opts = {},
 }
