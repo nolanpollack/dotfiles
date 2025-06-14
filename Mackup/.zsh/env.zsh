bfe31737439f63825e6709328845c152adfc036c
Nolan Pollack <nolanfpollack@gmail.com>
2025-06-14 17:00:36 -0400

    use homebrew prefix

--- Mackup/.zsh/env.zsh
+++ Mackup/.zsh/env.zsh
@@ -4,7 +4,7 @@ export ZSH="$HOME/.oh-my-zsh"
 export MANPAGER='nvim +Man!'
 export EDITOR=nvim
 export COLORTERM='truecolor' 
-export SUDO_EDITOR=/home/linuxbrew/.linuxbrew/bin/nvim
+export SUDO_EDITOR="$HOMEBREW_PREFIX/bin/nvim"
 path+=("$HOME/.local/bin")
 path+=("$HOME/.ebcli-virtual-env/executables")
 
