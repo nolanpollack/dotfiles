2242180b3d46fdc023e0cfb5c62c619d8f756aff
Nolan Pollack <nolanfpollack@gmail.com>
2025-12-01 11:00:36 -0500

    remove avante from markview

--- Mackup/.config/nvim/lua/nolan/plugins/markview.lua
+++ Mackup/.config/nvim/lua/nolan/plugins/markview.lua
@@ -1,9 +1,10 @@
 return {
+    -- TODO: Disable by default with keybind to preview in split mode
 	"OXY2DEV/markview.nvim",
 	dependencies = {
 		"echasnovski/mini.icons",
 	},
-	ft = { "markdown", "Avante" },
+	ft = { "markdown" },
 	opts = {
 		preview = {
 			filetypes = { "markdown", "Avante" },
