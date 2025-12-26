d9c0e735abdd048c7f220a75d291300428a8e09e
Nolan Pollack <nolanfpollack@gmail.com>
2025-12-26 12:13:58 -0500

    Text case

---
+++ nvim/.config/nvim/lua/plugins/text-case.lua
@@ -0,0 +1,8 @@
+return {
+	"johmsalas/text-case.nvim",
+	opts = {},
+	-- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+	-- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+	-- available after the first executing of it or after a keymap of text-case.nvim has been used.
+	lazy = false,
+}
