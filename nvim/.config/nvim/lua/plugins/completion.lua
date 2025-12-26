1539b44a057173f3296ca02fdd86f2093e910643
Nolan Pollack <nolanfpollack@gmail.com>
2025-12-26 15:30:17 -0500

    Better autocomplete with supermaven

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,60 +1,35 @@
 return {
-	{ "supermaven-inc/supermaven-nvim" },
+	{
+		"supermaven-inc/supermaven-nvim",
+		opts = {},
+	},
 	{
 		"saghen/blink.cmp",
 		version = "1.8.0",
-		dependencies = {
-			"saghen/blink.compat",
-			"supermaven-inc/supermaven-nvim",
-		},
 		-- @type blink.cmp.Config
 		opts = {
-			sources = {
-				providers = {
-					supermaven = {
-						name = "supermaven",
-						module = "blink.compat.source",
-					},
-				},
-			},
 			completion = {
 				menu = {
 					border = "rounded",
-
 					draw = {
 						columns = {
 							{ "kind_icon" },
 							{ "label", "label_description", gap = 1 },
-							{ "kind" }, -- Add this to show the completion type name
+							{ "kind" },
 						},
+						treesitter = { "lsp" },
 					},
-					-- Make sure menu doesn't overlap with ghost text (copilot)
-					direction_priority = function()
-						local ctx = require("blink.cmp").get_context()
-						local item = require("blink.cmp").get_selected_item()
-						if ctx == nil or item == nil then
-							return { "s", "n" }
-						end
-
-						local item_text = item.textEdit ~= nil and item.textEdit.newText
-							or item.insertText
-							or item.label
-						local is_multi_line = item_text:find("\n") ~= nil
-
-						-- after showing the menu upwards, we want to maintain that direction
-						-- until we re-open the menu, so store the context id in a global variable
-						if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
-							vim.g.blink_cmp_upwards_ctx_id = ctx.id
-							return { "n", "s" }
-						end
-						return { "s", "n" }
-					end,
+					direction_priority = { "n", "s" },
 				},
 				list = {
 					selection = {
 						preselect = false,
 						auto_insert = false,
 					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
 				},
 			},
 			signature = { enabled = true },
