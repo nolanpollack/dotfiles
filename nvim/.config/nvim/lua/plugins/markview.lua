272e668644e7d828fbcaef12edc068fa64ff9a79
Nolan Pollack <nolanfpollack@gmail.com>
2026-01-20 12:54:12 -0500

    Disable markview by default

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -5,10 +5,16 @@ return {
 		"echasnovski/mini.icons",
 	},
 	ft = { "markdown" },
+    keys = {
+        { "<leader>mv", "<cmd>Markview splitToggle<cr>", desc = "Toggle Markview Preview" },
+    },
 	opts = {
 		preview = {
 			filetypes = { "markdown", "Avante" },
             ignore_buftypes = {},
 		},
+        markdown = {
+            enable = false,
+        }
 	},
 }
