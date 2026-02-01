8de69885ce4c706f7b62d894b16000d4dd8df1b9
Nolan Pollack <nolanfpollack@gmail.com>
2026-02-01 18:26:48 -0500

    Prettier for markdown

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,67 +1,71 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	keys = {
-		{
-			"<leader>mp",
-			function()
-				require("conform").format({
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    keys = {
+        {
+            "<leader>mp",
+            function()
+                require("conform").format({
                     lsp_format = "first",
-					async = true,
-					timeout_ms = 1000,
-				})
-			end,
-			{ "n", "v" },
-			desc = "Make Pretty (Format file or range in visual mode)",
-		},
-	},
-	opts = function()
-		-- Use two spaces for indentation with prettier-managed files
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"typescript",
-				"typescriptreact",
-				"javascript",
-				"javascriptreact",
-				"json",
-				"css",
-				"html",
-				"markdown",
-				"yaml",
-			},
-			callback = function()
-				vim.opt_local.shiftwidth = 2
-				vim.opt_local.tabstop = 2
-				vim.opt_local.softtabstop = 2
-			end,
-		})
-		return {
-			formatters_by_ft = {
-				css = { "prettier" },
-				html = { "prettier" },
-				groovy = { "npm-groovy-lint" },
-				java = { "google-java-format" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				json = { "prettier" },
-				lua = { "stylua" },
-				markdown = { "prettier" },
-				python = { "isort", "black" },
-				r = { "styler" },
-				rust = { "rustfmt" },
-				sh = { "shellcheck" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				yaml = { "prettier" },
-			},
-			formatters = {
-				["google-java-format"] = {
-					prepend_args = { "--aosp" },
-				},
+                    async = true,
+                    timeout_ms = 1000,
+                })
+            end,
+            { "n", "v" },
+            desc = "Make Pretty (Format file or range in visual mode)",
+        },
+    },
+    opts = function()
+        -- Use two spaces for indentation with prettier-managed files
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+                "typescript",
+                "typescriptreact",
+                "javascript",
+                "javascriptreact",
+                "json",
+                "css",
+                "html",
+                "markdown",
+                "yaml",
+            },
+            callback = function()
+                vim.opt_local.shiftwidth = 2
+                vim.opt_local.tabstop = 2
+                vim.opt_local.softtabstop = 2
+            end,
+        })
+        return {
+            formatters_by_ft = {
+                css = { "prettier" },
+                html = { "prettier" },
+                groovy = { "npm-groovy-lint" },
+                java = { "google-java-format" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                json = { "prettier" },
+                lua = { "stylua" },
+                markdown = { "prettier_no_cwd" },
+                python = { "isort", "black" },
+                r = { "styler" },
+                rust = { "rustfmt" },
+                sh = { "shellcheck" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                yaml = { "prettier" },
+            },
+            formatters = {
+                ["google-java-format"] = {
+                    prepend_args = { "--aosp" },
+                },
                 prettier = {
                     require_cwd = true,
-                }
-			},
-		}
-	end,
+                },
+                prettier_no_cwd = {
+                    inherit = "prettier",
+                    require_cwd = false,
+                },
+            },
+        }
+    end,
 }
