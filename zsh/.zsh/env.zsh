bb09859dd02d2b730e8513c3fdeac9af1216df31
Nolan Pollack <nolanfpollack@gmail.com>
2026-04-14 13:08:57 -0400

    Update env

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -1,9 +1,16 @@
 # Environment variables
+typeset -U path
+
 export MANPAGER='nvim +Man!'
 export EDITOR=nvim
 export COLORTERM='truecolor' 
 export SUDO_EDITOR="$HOMEBREW_PREFIX/bin/nvim"
+
+# Path
 path+=("$HOME/.local/bin")
 path+=("$HOME/.ebcli-virtual-env/executables")
-path+=($(brew --prefix rustup)/bin)
+path+=("$HOMEBREW_PREFIX/opt/rustup/bin")
 path+=("$HOME/.cargo/bin")
+
+# Enable tool search in claude to avoid loading all mcps
+ENABLE_TOOL_SEARCH=true
