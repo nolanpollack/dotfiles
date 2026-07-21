c43119a6c33d03154b8bca512349ced30767f715
Nolan Pollack <nolanfpollack@gmail.com>
2026-07-21 10:15:12 -0400

    new treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,78 +1,76 @@
--- TODO: Ensure this is configured correctly for neovim 11. Is there any way to ensure it's async?
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
+	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-	init = function()
-		vim.treesitter.language.register("bash", "zsh")
-		vim.treesitter.language.register("bash", "tmux")
-		vim.treesitter.language.register("java", "aidl")
-		vim.filetype.add({
-			filename = {
-				["apple-app-site-association"] = "json",
-			},
-		})
-		vim.api.nvim_create_augroup("filetype_overrides", { clear = true })
-		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-			group = "filetype_overrides",
-			pattern = "*.pac",
-			callback = function()
-				vim.bo.filetype = "pac"
-			end,
-		})
-		vim.treesitter.language.register("javascript", "pac")
-	end,
-	keys = {
-		{ "<S-M-l>", "<cmd>TSTextobjectSwapNext @parameter.inner<CR>", desc = "Swap to previous parameter" },
-	},
-	main = "nvim-treesitter.configs",
-	opts = {
-		-- automatically install missing parsers when entering buffer
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		ensure_installed = {
-			"latex",
-			"regex",
-		},
-		-- enable indentation
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<CR>",
-				node_incremental = "<CR>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				-- Automatically jump forward to textobj, similar to targets.vim
-				lookahead = true,
-				keymaps = {
-					["af"] = { query = "@function.outer", desc = "outer function" },
-					["if"] = { query = "@function.inner", desc = "inner function" },
-
-					["aa"] = { query = "@parameter.outer", desc = "outer argument" },
-					["ia"] = { query = "@parameter.inner", desc = "inner argument" },
-
-					["ac"] = { query = "@class.outer", desc = "outer class" },
-					["ic"] = { query = "@class.inner", desc = "inner class" },
-
-					["igc"] = { query = "@comment.outer", desc = "inner comment" },
-					["agc"] = { query = "@comment.outer", desc = "outer comment" },
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = { query = "@function.outer", desc = "outer function" },
+						["if"] = { query = "@function.inner", desc = "inner function" },
+						["aa"] = { query = "@parameter.outer", desc = "outer argument" },
+						["ia"] = { query = "@parameter.inner", desc = "inner argument" },
+						["ac"] = { query = "@class.outer", desc = "outer class" },
+						["ic"] = { query = "@class.inner", desc = "inner class" },
+						["igc"] = { query = "@comment.outer", desc = "inner comment" },
+						["agc"] = { query = "@comment.outer", desc = "outer comment" },
+					},
+					selection_modes = {
+						["@function.outer"] = "V",
+					},
 				},
-				selection_modes = {
-					["@function.outer"] = "V", -- linewise
-				},
-			},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 		},
+		lazy = false,
+		init = function()
+			vim.treesitter.language.register("bash", "zsh")
+			vim.treesitter.language.register("bash", "tmux")
+			vim.treesitter.language.register("java", "aidl")
+			vim.filetype.add({
+				filename = {
+					["apple-app-site-association"] = "json",
+				},
+			})
+			vim.api.nvim_create_augroup("filetype_overrides", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				group = "filetype_overrides",
+				pattern = "*.pac",
+				callback = function()
+					vim.bo.filetype = "pac"
+				end,
+			})
+			vim.treesitter.language.register("javascript", "pac")
+		end,
+		keys = {
+			{ "<S-M-l>", "<cmd>TSTextobjectSwapNext @parameter.inner<CR>", desc = "Swap to previous parameter" },
+		},
+		config = function()
+			require("nvim-treesitter").setup({
+				auto_install = true,
+				ensure_installed = { "latex", "regex" },
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+		end,
 	},
 }
